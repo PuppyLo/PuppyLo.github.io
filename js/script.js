54a4e255e6480aaf35577c9aabe46f6a8f1f636c
PuppyLo <g-rasul@mail.ru>
2024-04-07 13:32:39 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $('.header__burger, .header__menu').toggleClass('active');
-        $('.body').toggleClass('lock');
+        $('body').toggleClass('lock');
     });
 });
 
 $(document).ready(function () {
     $('.footer__burger').click(function (event) {
         $('.footer__burger, .footer__menu').toggleClass('active');
-        $('.body').toggleClass('lock');
+        $('body').toggleClass('lock');
     });
 });
