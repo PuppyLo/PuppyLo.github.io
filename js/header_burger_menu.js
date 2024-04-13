616738e7e7e4090129f8bc3217ad3a145268d111
PuppyLo <g-rasul@mail.ru>
2024-04-13 10:40:38 +0300

    update

--- js/header_burger_menu.js
+++ js/header_burger_menu.js
@@ -4,3 +4,10 @@ $(document).ready(function () {
         $('body').toggleClass('lock');
     });
 });
+
+$(document).ready(function () {
+    $('.header__list_menu').click(function (event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
